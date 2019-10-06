97e7e090926bee577662004b74718ca37d4e5b6d
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-10-06 12:41:35 +0200

    Minor cleanup

--- server/src/HTTPServer.ts
+++ server/src/HTTPServer.ts
@@ -2,25 +2,24 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express, { Express } from "express";
 import helmet from "helmet";
-import http, { Server as httpServer } from "http";
-import fs from "fs";
+import http, { Server } from "http";
 
 import { MainRouter } from "./router";
 
-export default class HTTPServer {
-	expressApp: Express;
-	httpServer: httpServer;
-	
-	constructor(port: string = "8080") {
-		this.expressApp = express();
-		
-		this.expressApp.use(helmet());
-		this.expressApp.use(cors());
-		this.expressApp.use(bodyParser.json());
-		this.expressApp.use("/", MainRouter);
-		
-		this.httpServer = http.createServer(this.expressApp).listen(port, () => {
-			console.log(`\x1b[34m[INFO]\x1b[0m Valhalla started on port ${process.env.SERVER_PORT}!`)
-		});
-	}
+export class HTTPServer {
+    expressApp: Express;
+    httpServer: Server;
+
+    constructor(port: number = 8080) {
+        this.expressApp = express();
+
+        this.expressApp.use(helmet());
+        this.expressApp.use(cors());
+        this.expressApp.use(bodyParser.json());
+        this.expressApp.use("/", MainRouter);
+
+        this.httpServer = http.createServer(this.expressApp).listen(port, () => {
+            console.log(`\x1b[34m[INFO]\x1b[0m Valhalla started on port ${port}!`);
+        });
+    }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,7 @@
-import bodyParser from "body-parser";
-import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
-import helmet from "helmet";
-import http from "http";
 
 dotenv.config();
 
-//#region HTTP Server
-import HTTPServer from "./HTTPServer"
-const server = new HTTPServer(process.env.SERVER_PORT);
-//#endregion
+import { HTTPServer } from "./HTTPServer";
+
+const server = new HTTPServer(+process.env.SERVER_PORT);
