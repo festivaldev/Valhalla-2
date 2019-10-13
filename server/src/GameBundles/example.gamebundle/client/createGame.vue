48e6864a23ed52b08f6d788f734961f9420b995c
SniperGER <janikschmidt@icloud.com>
2019-10-13 14:03:31 +0200

    Added automatic parsing as numbers

--- server/src/GameBundles/example.gamebundle/client/createGame.vue
+++ server/src/GameBundles/example.gamebundle/client/createGame.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
-		<MetroTextBox header="Spieler-Limit" :placeholder-text="defaultGameOptions.playerLimit.toString()" v-model="gameOptions.playerLimit" />
-		<MetroTextBox header="Zuschauer-Limit" :placeholder-text="defaultGameOptions.spectatorLimit.toString()" v-model="gameOptions.spectatorLimit" />
-		<MetroTextBox header="Punkte-Limit" :placeholder-text="defaultGameOptions.scoreLimit.toString()" v-model="gameOptions.scoreLimit" />
+		<MetroTextBox header="Spieler-Limit" :placeholder-text="defaultGameOptions.playerLimit.toString()" v-model.number="gameOptions.playerLimit" />
+		<MetroTextBox header="Zuschauer-Limit" :placeholder-text="defaultGameOptions.spectatorLimit.toString()" v-model.number="gameOptions.spectatorLimit" />
+		<MetroTextBox header="Punkte-Limit" :placeholder-text="defaultGameOptions.scoreLimit.toString()" v-model.number="gameOptions.scoreLimit" />
 		
 		<MetroCheckBox v-model="gameOptions.exampleProperty" content="Beispiel-Eigenschaft" />
 	</div>
