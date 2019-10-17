2c9322ab78ea7c11ad547577ff9d0d53f8f542ec
SniperGER <janikschmidt@icloud.com>
2019-10-17 11:30:07 +0200

    Added client resources for CAH

---
+++ server/src/GameBundles/CAH.gamebundle/client/client.js
@@ -0,0 +1,20 @@
+/**
+ * Cards Against Humanity
+ * Client Script
+ * 
+ * © 2019 Team FESTIVAL
+ */
+
+(() => {
+	Object.assign(window.gameBundles["CAHGameBundle"], {
+		defaultGameOptions: {
+			scoreLimit: 8,
+			playerLimit: 10,
+			spectatorLimit: 10,
+			blanksInDeck: 0
+		},
+		startGame: () => {
+			
+		}	
+	});
+})()
---
+++ server/src/GameBundles/CAH.gamebundle/client/createGame.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<MetroTextBox header="Punkte-Limit" :placeholder-text="defaultGameOptions.scoreLimit.toString()" v-model.number="gameOptions.scoreLimit" />
+		
+		<MetroTextBlock class="mb-3" text-style="sub-title">Decks</MetroTextBlock>
+		<template v-if="!decks.length">
+			<MetroProgressRing :active="true" />
+		</template>
+		<template v-if="decks.length">
+			<p v-for="(deck, index) in decks" :key="index" :title="deck.description">{{ deck.name }}</p>
+		</template>
+	</div>
+</template>
+
+<script>
+const axios = require("axios");
+const SocketService = require("SocketService");
+
+module.exports = {
+	name: "CAHCreateGame",
+	data: () => ({
+		decks: []
+	}),
+	async mounted() {
+		this.decks = await axios.get(`${SocketService.url}/cah/decks`).then(response => response.data);
+		console.log(this.decks);
+	},
+	methods: {
+		test() {
+			alert("Test");
+		}
+	},
+	computed: {
+		gameOptions() {
+			return this.$parent.gameOptions;
+		},
+		defaultGameOptions() {
+			return window.gameBundles["CAHGameBundle"].defaultGameOptions;
+		}
+	}
+}
+</script>
