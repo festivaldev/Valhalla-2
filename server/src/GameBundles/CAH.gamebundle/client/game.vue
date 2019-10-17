921318620dc76a6b5716051a4061c613a6092d8f
SniperGER <janikschmidt@icloud.com>
2019-10-17 23:48:40 +0200

    Added CAH client prototype

---
+++ server/src/GameBundles/CAH.gamebundle/client/game.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<MetroTextBlock text-style="header">CAH Prototype</MetroTextBlock>
+		<MetroButton @click="startGame">Start Game (Test)</MetroButton>
+	</div>
+</template>
+
+<script>
+const SocketService = require("SocketService");
+
+module.exports = {
+	name: "CAHGame",
+	methods: {
+		startGame() {
+			SocketService.emit({type:"start-game"})
+		},
+		handleGameEvent(message) {
+			console.log(message);
+		}
+	}
+}
+</script>
