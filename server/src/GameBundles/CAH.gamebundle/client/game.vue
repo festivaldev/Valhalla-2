1ef0c02c7c62c6ac4cb2f26c814f0dfd148143e6
SniperGER <janikschmidt@icloud.com>
2019-10-18 11:24:24 +0200

    Started working on actual CAH gameplay

--- server/src/GameBundles/CAH.gamebundle/client/game.vue
+++ server/src/GameBundles/CAH.gamebundle/client/game.vue
@@ -1,21 +1,39 @@
 <template>
 	<div>
-		<MetroTextBlock text-style="header">CAH Prototype</MetroTextBlock>
-		<MetroButton @click="startGame">Start Game (Test)</MetroButton>
+		<template v-if="currentGame.state == CAHGameState.LOBBY">
+			<MetroTextBlock text-style="header">Game Lobby</MetroTextBlock>
+			
+			<MetroButton :disabled="currentGame.players.length < 3">Start Game</MetroButton>
+		</template>
 	</div>
 </template>
 
 <script>
+const CAHGameState = {
+	JUDGING: "judging",
+	LOBBY: "lobby",
+	PLAYING: "playing",
+	ROUND_OVER: "round-over"
+};
+
 const SocketService = require("SocketService");
 
 module.exports = {
 	name: "CAHGame",
+	data: () => ({
+		
+	}),
 	methods: {
 		startGame() {
 			SocketService.emit({type:"start-game"})
 		},
 		handleGameEvent(message) {
 			console.log(message);
+		}
+	},
+	computed: {
+		CAHGameState() {
+			return CAHGameState;
 		}
 	}
 }
